76362b439987fe6a651aa7b2d520c2fb8a834677
madebyKir <melomah@mail.ru>
2025-06-16 01:12:45 +0300

    Update install.sh
    ScreensaverManager - >ScreenSaverManager

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
-mkdir -p "/mnt/us/extensions/ScreensaverManager/backup_screensavers"
-cp /usr/share/blanket/screensaver/*.png /mnt/us/extensions/ScreensaverManager/backup_screensavers/ 2>/dev/null
+mkdir -p "/mnt/us/extensions/ScreenSaverManager/backup_screensavers"
+cp /usr/share/blanket/screensaver/*.png /mnt/us/extensions/ScreenSaverManager/backup_screensavers/ 2>/dev/null
 echo "Готово! Добавьте PNG-файлы в папку screens/"
