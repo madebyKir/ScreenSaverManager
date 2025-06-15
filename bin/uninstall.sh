fac14d185446624b7c35096444cfac824b720940
madebyKir <melomah@mail.ru>
2025-06-16 01:14:19 +0300

    Update uninstall.sh
    ScreensaverManager -> ScreenSaverManager

--- bin/uninstall.sh
+++ bin/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 mntroot rw
-cp -f /mnt/us/extensions/ScreensaverManager/backup_screensavers/*.png /usr/share/blanket/screensaver/
+cp -f /mnt/us/extensions/ScreenSaverManager/backup_screensavers/*.png /usr/share/blanket/screensaver/
 mntroot ro
 lipc-set-prop com.lab126.powerd refreshScreenSaver 1
 echo "Оригинальные скринсейверы восстановлены!"
